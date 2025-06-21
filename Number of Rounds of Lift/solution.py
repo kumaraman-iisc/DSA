22e42c38507ba49d7052b530950248f8d4e1cba4
Kumar Aman <kumaraman@iisc.ac.in>
2025-06-21 09:02:55 +0530

    Create solution.py

---
+++ Number of Rounds of Lift/solution.py
@@ -0,0 +1,19 @@
+def calculate_lift_rounds(n, capacity):
+    """
+    Function to calculate the number of rounds the lift needs to cover.
+    
+    Parameters:
+    n (int): Total number of people.
+    capacity (int): Maximum number of people the lift can carry in one round.
+    
+    Returns:
+    int: The number of rounds required to transport all people to the top floor.
+    """
+    # Your code here
+    rounds = n // capacity
+    
+    if n%capacity != 0:
+        return rounds + 1
+        
+    else:
+        return rounds
