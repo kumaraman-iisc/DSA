c01c5020c5ea8451d76aa75b4cefe7b45f673ea5
Kumar Aman <kumaraman@iisc.ac.in>
2025-06-21 12:13:31 +0530

    Create solution2.py

---
+++ Inbuilt data structures/Reverse a List/solution2.py
@@ -0,0 +1,9 @@
+def reverse_list(lst):
+    # Your code goes here
+    reversed_list = []
+    
+    for num in reversed(lst):
+        reversed_list.append(num)
+        
+    return reversed_list
+    pass
