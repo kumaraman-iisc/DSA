e5d1d45175fb5431c111861e19b7bfebe6688635
Kumar Aman <kumaraman@iisc.ac.in>
2025-06-22 11:41:10 +0530

    Create solution.py

---
+++ Maths Practice Questions/Sum of N Even Natural Numbers/solution.py
@@ -0,0 +1,17 @@
+def sum_of_even_numbers(n):
+    """
+    Function to return the sum of the first n even natural numbers.
+    
+    Parameters:
+    n (int): The number of even numbers to sum.
+    
+    Returns:
+    int: The sum of the first n even natural numbers.
+    """
+    # Your code here
+  add = 0
+
+  for i in range(1, n+1):
+    add += 2 * i
+
+  return add
