71cf196426f4a7d421a2733f5726b6fcdf6bebf9
Kumar Aman <kumaraman@iisc.ac.in>
2025-06-25 00:21:28 +0530

    Create solution.py

---
+++ List and Arrays Practice Questions/Sum of Elements in a List/solution.py
@@ -0,0 +1,14 @@
+def sum_of_elements(lst):
+    """
+    Function to find the sum of all elements in the list.
+    :param lst: List[int] -> List of integers
+    :return: int -> The sum of all elements in the list
+    """
+    # TODO: Implement this function
+    add = 0
+    
+    for i in lst:
+        add += i
+        
+    return add
+    pass
